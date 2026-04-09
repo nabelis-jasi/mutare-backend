c0dc378b27c345df6a83e47e917f703e64a14d4f
nabelis-jasi <nabelisj65@gmail.com>
2026-04-09 08:56:31 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
+  if (!token) return res.status(401).json({ error: 'No token provided' });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(401).json({ error: 'Invalid token.' });
+  } catch {
+    res.status(401).json({ error: 'Invalid token' });
   }
 };
