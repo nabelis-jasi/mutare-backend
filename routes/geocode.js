10ec33288d1081573faa864aa6ea50cee8a323e8
nabelis-jasi <nabelisj65@gmail.com>
2026-04-09 15:36:48 +0200

    Update geocode.js

--- routes/geocode.js
+++ routes/geocode.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const OpenLocationCode = require('"open-location-code": "1.0.4"');
+const OpenLocationCode = require('open-location-code');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
