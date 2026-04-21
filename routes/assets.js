e97cede7a1c7c81e40aa94e1c509c0037e438628
nabelis-jasi <nabelisj65@gmail.com>
2026-04-21 11:11:47 +0200

    Update assets.js

--- routes/assets.js
+++ routes/assets.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const db = require('../node/config/db');
 
 // Get all assets
 router.get('/', async (req, res) => {
