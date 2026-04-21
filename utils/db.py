9e2d427a1a780f6222d8a70d7f64ce61ebe52bee
nabelis-jasi <nabelisj65@gmail.com>
2026-04-21 10:55:57 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,14 @@
 
+import os
+import psycopg2
+import psycopg2.extras
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv('PG_HOST'),
+        port=os.getenv('PG_PORT'),
+        user=os.getenv('PG_USER'),
+        password=os.getenv('PG_PASSWORD'),
+        database=os.getenv('PG_DATABASE'),
+        cursor_factory=psycopg2.extras.RealDictCursor
+    )
