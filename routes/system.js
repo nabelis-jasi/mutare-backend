b3596baa784d9ea0ddb9af94ecb6bbec2f719fd2
nabelis-jasi <nabelisj65@gmail.com>
2026-04-21 11:42:32 +0200

    Update system.js

--- routes/system.js
+++ routes/system.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../node/db.config');
+const db = require('../node/config/db');
 
 router.get('/db-status', async (req, res) => {
   try {
